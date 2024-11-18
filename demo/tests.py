aacc47118c302ab3070fc07487c1a0a91c1e8e67
Manjunatha K <mkubasad100@gmail.com>
2024-11-18 21:08:32 +0530

    Initial commit

---
+++ demo/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+class dummyTestCase1(TestCase):
+    def test1(self):
+        self.assertEqual(1, 2)
+
+class dummyTestCase2(TestCase):
+    def test1(self):
+        self.assertEqual(1, 2)
