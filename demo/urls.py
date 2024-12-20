d1c518733e24afa750287cc94c6abab9308257d7
Manjunatha K <mkubasad100@gmail.com>
2024-12-21 00:28:34 +0530

    Added models

--- demo/urls.py
+++ demo/urls.py
@@ -7,4 +7,5 @@ app_name = 'demo'
 urlpatterns = [
     path("", views.home, name="home"),
     path("home/", views.home, name="home"),
+    path("addDoctor/", views.createDoctor, name="addDoctor"),
 ]
